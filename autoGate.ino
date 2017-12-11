e856d3eeed692d0dde2fe179964d0be99aaea1f0
Sid Narayan <sidsdn@gmail.com>
2017-12-10 20:12:08 -0800

    Added liner actuator positional feedback and SoftwareSerial

--- autoGate.ino
+++ autoGate.ino
@@ -1,35 +1,71 @@
-int extendPin = 2;
-int retractPin = 3;
+#include <SoftwareSerial.h>
+
+#define EXTEND 0
+#define RETRACT 1
+#define HOLD 2
+
+int sensorPin = 2;
+SoftwareSerial bluetooth(4,5);
+int extendPin = 6;
+int retractPin = 7;
 String data;
-int state = LOW;
+int pos = 0;
+int state = HOLD;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(sensorPin, INPUT_PULLUP);
   pinMode(extendPin, OUTPUT);
   pinMode(retractPin, OUTPUT);
   digitalWrite(extendPin, HIGH);
   digitalWrite(retractPin, HIGH);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), pulse, RISING); 
   Serial.begin(9600);
+  bluetooth.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(Serial.available()) {
-    delay(6);  
-    char c = Serial.read();
-    data += c; 
+  bluetooth.listen();
+  while (bluetooth.available()) {
+    delay(6);
+    char c = bluetooth.read();
+    data += c;
   }
-  if(data == "open") {
+  if (data == "open" && pos > 0) {
     digitalWrite(retractPin, LOW);
     digitalWrite(extendPin, HIGH);
+    state = RETRACT;
   }
-  else if (data == "close") {
+  else if (data == "close" && pos < 852) {
     digitalWrite(retractPin, HIGH);
     digitalWrite(extendPin, LOW);
+    state = EXTEND;
   }
   else if (data == "stop") {
     digitalWrite(retractPin, HIGH);
     digitalWrite(extendPin, HIGH);
+    state = HOLD;
+  }
+  if (pos >= 852 && state == EXTEND) {
+    digitalWrite(retractPin, HIGH);
+    digitalWrite(extendPin, HIGH);
+    state = HOLD;
+  }
+  else if (pos <= 0 && state == RETRACT) {
+    digitalWrite(retractPin, HIGH);
+    digitalWrite(extendPin, HIGH);
+    state = HOLD;    
   }
   data = "";
 }
+
+void pulse(){
+  if(state == EXTEND) {
+    pos++;
+  }
+  else if(state == RETRACT) {
+    pos--;
+  }
+}
+
